7169f3e51b09143e738605236822a0b6779d3391
BagasBerlian <indonesia4gaming@gmail.com>
2024-07-18 12:17:16 +0700

    initialize commit

---
+++ src/config/products.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_PRICES = {
+  material: {
+    silicone: 0,
+    polycarbonate: 30_000_00,
+  },
+  finish: {
+    smooth: 0,
+    textured: 20_000_00,
+  },
+} as const;
+
+export const BASE_PRICE = 35_000_00;
