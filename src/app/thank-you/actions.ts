be6b58f8790f1fb13298ca7f4619365a7d97c054
BagasBerlian <indonesia4gaming@gmail.com>
2024-07-26 06:49:11 +0700

    try to fix thank-you page in domain vercel view

--- src/app/thank-you/actions.ts
+++ src/app/thank-you/actions.ts
@@ -2,10 +2,11 @@
 
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { KindeUser } from "@kinde-oss/kinde-auth-nextjs/types";
 
-export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+export const getPaymentStatus = async ({ orderId, user }: { orderId: string; user: KindeUser }) => {
+  // const { getUser } = getKindeServerSession();
+  // const user = await getUser();
 
   if (!user?.id || !user.email) {
     throw new Error("You need to be logged in to view this page");
