815817d0cdb91c8dd407eb23f49deeb7f0ab39a9
BagasBerlian <indonesia4gaming@gmail.com>
2024-07-25 14:33:46 +0700

    updated: preview/actions.ts

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -36,6 +36,8 @@ export const createCheckoutSession = async ({ configId }: { configId: string }) 
     },
   });
 
+  console.log(configId, configuration.id);
+
   if (existingOrder) {
     order = existingOrder;
   } else {
@@ -60,7 +62,7 @@ export const createCheckoutSession = async ({ configId }: { configId: string }) 
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ["grabpay"],
+    payment_method_types: ["card", "grabpay"],
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["ID", "US"] },
     metadata: {
