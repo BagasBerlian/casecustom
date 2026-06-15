6c78a95dd6f2b63864db008446d684e4b238db84
BagasBerlian <indonesia4gaming@gmail.com>
2026-06-15 20:24:41 +0700

    fix bug build when change the max file size upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,7 +6,7 @@ import { db } from "@/db";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "7MB" } })
+  imageUploader: f({ image: { maxFileSize: "8MB" } })
     .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
       return { input };
